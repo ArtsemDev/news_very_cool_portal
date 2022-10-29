7f81d7cd3032cce6c84fee6599fe306ac480003d
ArtsemDev <thommy.python.dev@gmail.com>
2022-10-29 12:38:07 +0300

    conchenyi authentication

--- not_clean/api/urls.py
+++ not_clean/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from .views import PostViewSet
+from .views import PostViewSet, expired_obtain_auth_token
 
 
 router = SimpleRouter()
@@ -9,5 +9,6 @@ router.register(r'posts', PostViewSet)
 
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('auth/', expired_obtain_auth_token)
 ]
